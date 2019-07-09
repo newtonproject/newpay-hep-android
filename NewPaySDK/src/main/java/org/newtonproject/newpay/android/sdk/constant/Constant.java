699e9e95d95fdabe561b749ab5161bc2071e87f9
weixuefeng <weixuefeng@lubangame.com>
2019-07-09 20:10:01 +0800

    update host

--- NewPaySDK/src/main/java/org/newtonproject/newpay/android/sdk/constant/Constant.java
+++ NewPaySDK/src/main/java/org/newtonproject/newpay/android/sdk/constant/Constant.java
@@ -6,26 +6,26 @@ package org.newtonproject.newpay.android.sdk.constant;
 public class Constant {
 
     public interface MainNet {
-        String authorize_pay = "newpay://org.newtonproject.newpay.android.release.authorize2";
-        String authorize_login_place = "newpay://org.newtonproject.newpay.android.release.authorize2";
+        String authorize_pay = "newpay://org.newtonproject.newpay.android.release.authorize_v2";
+        String authorize_login_place = "newpay://org.newtonproject.newpay.android.release.authorize_v2";
         String share_url = "https://www.newtonproject.org/download/newpay/mainnet/";
     }
 
     public interface TestNet {
-        String authorize_pay = "newpay://org.newtonproject.newpay.android.authorize2";
-        String authorize_login_place = "newpay://org.newtonproject.newpay.android.authorize2";
+        String authorize_pay = "newpay://org.newtonproject.newpay.android.authorize_v2";
+        String authorize_login_place = "newpay://org.newtonproject.newpay.android.authorize_v2";
         String share_url = "https://developer.newtonproject.org/download/newpay/testnet/";
     }
 
     public interface BetaNet {
-        String authorize_pay = "newpay://org.newtonproject.newpay.android.beta.authorize2";
-        String authorize_login_place = "newpay://org.newtonproject.newpay.android.beta.authorize2";
+        String authorize_pay = "newpay://org.newtonproject.newpay.android.beta.authorize_v2";
+        String authorize_login_place = "newpay://org.newtonproject.newpay.android.beta.authorize_v2";
         String share_url = "https://developer.newtonproject.org/download/newpay/betanet/";
     }
 
     public interface DevNet {
-        String authorize_pay = "newpay://org.newtonproject.newpay.android.dev.authorize2";
-        String authorize_login_place = "newpay://org.newtonproject.newpay.android.dev.authorize2";
+        String authorize_pay = "newpay://org.newtonproject.newpay.android.dev.authorize_v2";
+        String authorize_login_place = "newpay://org.newtonproject.newpay.android.dev.authorize_v2";
         String share_url = "https://developer.newtonproject.org/download/newpay/devnet/";
     }
 
